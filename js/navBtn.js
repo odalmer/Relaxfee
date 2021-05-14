5e86284dbb44703e2b4e83626ab9565122139aff
odalmer <48610363+odalmer@users.noreply.github.com>
2021-05-14 17:10:41 -0400

    initialize project files

---
+++ js/navBtn.js
@@ -0,0 +1,14 @@
+// Storing DOM elements
+const mobileNavBtn = document.querySelector("#mobileNavBtn");
+const navItem = document.querySelector("nav");
+
+let btnClose = false;
+mobileNavBtn.addEventListener("click", (e) => {
+  if (btnClose == false) {
+    navItem.style.background = "#2E294E";
+    btnClose = true;
+  } else {
+    navItem.style.background = "transparent";
+    btnClose = false;
+  }
+});
